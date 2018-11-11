63061f1296d2ff61cf5b77b84842a082667bc62c
datah <2282643301@qq.com>
2018-11-12 00:28:54 +0800

    Xadmin

--- django/xadmindemo/app01/Xadmin.py
+++ django/xadmindemo/app01/Xadmin.py
@@ -1,12 +1,17 @@
 
 from Xadmin.service.Xadmin import site
+from Xadmin.service.Xadmin import ModelXadmin
 
 
 print('app01 Xadmin')
 
 from app01.models import *
 
-site.register(Book)
+class BookConfig(ModelXadmin):
+    list_display = ["title","prcie"]
+
+
+site.register(Book,BookConfig)
 site.register(Publish)
 site.register(Author)
 site.register(AuthorDetail)
